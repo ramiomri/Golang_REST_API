79aa8b1fce544f46d12a3bd789c1ad43dbca0b22
ramiomri <ramiomri111@gmail.com>
2022-07-29 07:59:19 +0000

    api_rest

--- main.go
+++ main.go
@@ -1,1 +1,34 @@
 package main
+
+import (
+	"example/golang-rest-api/middleware"
+	"example/golang-rest-api/router"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "4000"
+	}
+
+	r := mux.NewRouter()
+	r.Use(middleware.Header)
+	router.RegisterRouter(r)
+
+	srv := &http.Server{
+		Handler:      r,
+		Addr:         "localhost:" + port,
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+	log.Println("Server started on port " + port)
+	log.Fatal(srv.ListenAndServe())
+}
